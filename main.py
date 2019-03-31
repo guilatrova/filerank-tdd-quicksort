28682c215d6d5cc7adabb1a345dbf69f62097301
Guilherme Latrova <guilhermelatrova@hotmail.com>
2019-03-31 16:05:08 -0300

    Refactor

--- main.py
+++ main.py
@@ -18,15 +18,20 @@ def trim_search_words(words):
     return set(words)
 
 
+def _calculate_result(found, total):
+    return (found * 100) / total
+
+
 def rank_files(words, files):
     results = {}
+
     for filename in files:
         found = 0
         for word in words:
             if word in files[filename]:
                 found += 1
 
-        results[filename] = (found * 100) / len(words)
+        results[filename] = _calculate_result(found, len(words))
 
     return results
 
