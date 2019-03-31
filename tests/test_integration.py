632de07a9f6b86de41ab5afc5b490123242d479f
Guilherme Latrova <guilhermelatrova@hotmail.com>
2019-03-31 15:21:58 -0300

    Extended integration test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -12,4 +12,5 @@ def test_integration_list_files():
     cur_path = os.path.dirname(os.path.realpath(__file__))
     result = read_files(cur_path)
 
-    assert "test_integration.py" in result
+    assert f"{cur_path}/test_integration.py" in result
+    assert "test_integration_list_files" in result["test_integration.py"]
